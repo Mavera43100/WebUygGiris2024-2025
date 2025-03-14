b0ffc6e11b11d8d7f160e6163ca20ce3cb8a4046
Mavera43100 <101385430+Mavera43100@users.noreply.github.com>
2025-03-14 22:35:14 +0000

    3sayıdan en büyüğü bulan code

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+
+// 3 sayıdan en büyüğünü bulan program
+$a = 5;
+$b = 60;
+$c = 15;
+
+if($a > $b && $a > $c){
+    echo "En büyük sayı: $a";
+}
+elseif($b > $a && $b > $c){
+    echo "En büyük sayı: $b";
+}
+else{
+    echo "En büyük sayı: $c";
+}   
+?>
+</body>
+</html>
